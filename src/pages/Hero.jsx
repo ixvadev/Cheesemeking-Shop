d5a5b6368f4589edbed01d69271677e7137a4a3a
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-07-29 14:05:53 +0500

    Update Hero.jsx

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Hero() {
   return (
-    <div className='contianer'>
-        <div className="flex gap-[5px]">
-            <div className="w-[590px] h-[300px] pt-[25px] bg-[url('./src/assets/svg/hero1.svg')] pl-[25px] text-white">
-                <p className="text-[36px] font-bold">Наборы для сыроделия</p>
-                <p className="mt-2 text-[18px] pb-[70px]">Все что вам нужно для приготовления сыра <br /> в домашних условиях</p>
-                <button className="w-[200px] h-[46px] bg-orange-500 hover:bg-[#FD9339] text-white text-[16px] py-2 px-4 rounded">Смотреть наборы</button>
+    <div className='container mx-auto px-4'>
+        <div className="flex flex-col gap-4 md:flex-row md:gap-5">
+            <div className="w-full md:w-[590px] h-[300px] pt-[25px] bg-[url('./src/assets/svg/hero1.svg')] pl-[25px] text-white flex flex-col justify-between">
+                <p className="text-[24px] md:text-[36px] font-bold">Наборы для сыроделия</p>
+                <p className="mt-2 text-[14px] md:text-[18px] pb-[40px] md:pb-[70px]">Все что вам нужно для приготовления сыра <br /> в домашних условиях</p>
+                <button className="w-full md:w-[200px] h-[46px] bg-orange-500 hover:bg-[#FD9339] text-white text-[14px] md:text-[16px] py-2 px-4 rounded">Смотреть наборы</button>
             </div>
-            <div className="w-[293px] h-[300px] bg-[url('./src/assets/svg/hero2.svg')] flex flex-col justify-center p-4 pt-[220px] text-white">
-                <p className="text-[16px] text-[#FD9339]">Рецепты</p>
-                <p className="text-[22px] font-bold">Сыры с плесенью</p>
+            <div className="w-full md:w-[293px] h-[300px] bg-[url('./src/assets/svg/hero2.svg')] flex flex-col justify-center p-4 md:pt-[220px] text-white">
+                <p className="text-[14px] md:text-[16px] text-[#FD9339]">Рецепты</p>
+                <p className="text-[18px] md:text-[22px] font-bold">Сыры с плесенью</p>
             </div>
-            <div className="w-[293px] h-[300px] bg-[url('./src/assets/svg/hero3.svg')] flex flex-col justify-center p-4 pt-[220px] text-white">
-                <p className="text-[16px] text-[#FD9339]">Акции</p>
-                <p className="text-[22px] font-bold">Закваски для сыра</p>
+            <div className="w-full md:w-[293px] h-[300px] bg-[url('./src/assets/svg/hero3.svg')] flex flex-col justify-center p-4 md:pt-[220px] text-white">
+                <p className="text-[14px] md:text-[16px] text-[#FD9339]">Акции</p>
+                <p className="text-[18px] md:text-[22px] font-bold">Закваски для сыра</p>
             </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
