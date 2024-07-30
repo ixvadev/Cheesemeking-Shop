19c54e75199bc25830d5ce03106f98a07571a2d8
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-07-30 15:26:42 +0500

    Preimu qo'shilmasi oldin

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Hero from './pages/Hero';
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./pages/Hero";
 
 import "@fontsource/ubuntu"; // Defaults to weight 400
 import "@fontsource/ubuntu/400.css"; // Specify weight
 import "@fontsource/ubuntu/400-italic.css"; // Specify weight and style
-import Ingredient from './components/Ingredient';
+import Ingredient from "./components/Ingredient";
+import axios from "axios";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [cards, setCards] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  const getCards = async () => {
+    setIsloading(true);
+    try {
+      const { data } = await axios.get("/public/card.json");
+      setCards(data.products);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsloading(false);
+    }
+  };
+  console.log(isLoading);
+  useEffect(() => {
+    getCards();
+  }, []);
 
   return (
     <>
       <Header />
-      <Hero/>
-      <Ingredient/>
+      <Hero />
+      <Ingredient />
+      {isLoading && (
+        <div className="w-10 h-10 border-black rounded-full border-4 border-dotted animate-spin" />
+      )}
+      {cards && cards.map((item) => <Card item={item} isLoading={isLoading} />)}
       <Footer />
     </>
   );
---
+++ src/pages/Preimu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Mashe() {
+  return (
+    <div className="bg-[url('/src/assets/svg/hero1.svg')]">
+        <div className=' '>
+            <div>
+                <p>Наши <br /> реимущества</p>
+                <p>Мы лично отвечаем за качество товара и всегда <br /> 
+                с радостью поможем Вам с выбором</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Mashe
