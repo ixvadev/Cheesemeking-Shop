3cce6ddd2337a9500e8933f45fc608f31704c9e9
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-07-31 16:37:16 +0500

    home muofiqqiyatle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,32 @@ const Home = () => {
                 <Card/>
             </div>
         </div>
+        <img src="/src/assets/svg/Features.svg" alt="" className='' />
         <Delya/>
+        
+        <div className='contianer py-[30px]'>
+            <p className='text-[#4E2D2D] text-[28px] font-bold my-3'>наше оборудование</p>
+            <div className="bg-[url('/src/assets/svg/nashe.svg')] p-[60px] h-[440px]">
+                <p className='text-[28px] uppercase text-[#4E2D2D] font-bold'>ПРЕССЫ ДЛЯ СЫРА</p>
+                <p className='text-[18px] py-[30px] text-[#79766E]'>Пресс можно использовать для прессования твердых <br /> сыров. Он разбирается, и вы можете компактно <br /> его упаковать в коробку.</p>
+                <button className='w-[160px] h-[44px] mt-[100px] text-[#ffffff] bg-[#FD9339]' >Узнать больше</button>
+            </div>
+        </div>
+        <div className='contianer'>
+            <div className='flex justify-center m-[50px]'>
+                <div className=''>
+                    <img src="/src/assets/svg/homecenter.svg" alt="" />
+                    <div className='p-[20px] bg-[#ffff]'>
+                        <p className='text-[#4E2D2D] text-[16px] font-bold py-[6px]'>Сырная тарелка</p>
+                        <p className='text-[#4E2D2D] text-[16px] py-[5px]'>Так называется блюдо, которое состоит <br /> из разных сортов сыра. </p>
+                        <div className='flex mt-[5px]'>
+                            <a href="" className='text-[#FD9339]'>Читать далее</a>
+                            <img src="/src/assets/svg/homeadd.svg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
