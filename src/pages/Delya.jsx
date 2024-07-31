0bb5d06339efe54e56cb55ed47c697e422f58a0d
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-07-31 11:13:34 +0500

    delya mufoqqiyatli bajarildi

--- src/pages/Delya.jsx
+++ src/pages/Delya.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Delya() {
   return (
-    <div>
-      
+    <div className='contianer'>
+      <div className='mt-[20px] mb-[10px]'>
+        <p className='text-[28px] text-[#4E2D2D] font-bold'>Мы подобрали для вас</p>
+      </div>
+      <div className='flex gap-[30px]'>
+        <div className="bg-[url('./src/assets/svg/bg1.svg')] w-[580px] h-[350px] p-[40px]">
+          <p className='text-[#4E2D2D] text-[25px] uppercase'>более 1000 лучших рецептов</p>
+          <p className='text-[#000000] text-[16px] py-[20px]'>Мы собрали для Вас большую <br />базу рецептов.</p>
+          <button className='mt-[110px] bg-[#FD9339] w-[132px] h-[44px] rounded-[5px] text-[16px] text-[#FFFFFF]'>Перейти</button>
+        </div>
+        <div className="bg-[url('./src/assets/svg/bg2.svg')] w-[580px] h-[350px] p-[40px]">
+          <p className='text-[#FFFFFF] text-[25px] uppercase' >крутые Наборы для сыра</p>
+          <p className='text-[#FFFFFF] text-[16px]  py-[20px]'>Готовые наборы сэкономят <br />время, мы всё сделали за Вас. </p>
+          <button className='mt-[110px] bg-[#FD9339] w-[132px] h-[44px] rounded-[5px] text-[16px] text-[#FFFFFF]'>Перейти</button>
+        </div>
+      </div>
     </div>
   )
 }
