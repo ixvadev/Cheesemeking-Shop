c85c671652b8edab2a48e3f5948eaf5f2815280b
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-08-02 00:24:28 +0500

    card add .

--- src/components/Cardinfo.jsx
+++ src/components/Cardinfo.jsx
@@ -5,8 +5,8 @@ function Cardinfo() {
         <div>
             <p>Мезофильная закваска Hansen Flora Danica (50U)</p>
             <div className='flex gap-[25px]'>
-                <img src="Cardinfo/Yulduzcha" alt="" />
-                <img src="Cardinfo/chat-messages" alt="" />
+                <img src="/src/assets/Cardinfo/Yulduzcha.svg" alt="" />
+                <img src="/src/assets/Cardinfo/chat-messages.svg" alt="" />
                 <p className='text-[13px] text-[#4E2D2D]'>2 Отзыва</p>
             </div>
         </div>
@@ -15,19 +15,19 @@ function Cardinfo() {
                 <div className='bg-[#ffffff]  rounded-[8px] my-[20px]'>
                     <img src="/src/assets/Cardinfo/cardinfo.svg" alt="" className='m-auto' />
                     <div className='flex justify-center gap-[15px] py-[10px]'>
-                        <img src="Cardinfo/info1" alt="" />
-                        <img src="Cardinfo/info2" alt="" />
-                        <img src="Cardinfo/info3" alt="" />
+                        <img src="/src/assets/Cardinfo/info1.svg" alt="" />
+                        <img src="/src/assets/Cardinfo/info2.svg" alt="" />
+                        <img src="/src/assets/Cardinfo/info3.svg" alt="" />
                     </div>
                     <div className='flex justify-center gap-[10px] py-[10px]'>
-                        <img src="Cardinfo/graduz320" alt="" />
+                        <img src="/src/assets/Cardinfo/graduz320.svg" alt="" />
                         <p className='text-[14px] text-[#808080]'>Посмотреть товар в 3D</p>
                     </div>
                 </div>
             </div>
 
             <div className='w-[470px] h-[220px]'>
-                <img className='mt-[20px]' src="Cardinfo/online" alt="" />
+                <img className='mt-[20px]' src="/src/assets/Cardinfo/online.svg" alt="" />
                 <div>
                     <div className='flex justify-between py-[5px]'>
                         <div className='flex'>
@@ -35,12 +35,12 @@ function Cardinfo() {
                             <p className='text-[#808080] text-[14px]'>1200 руб.</p>
                         </div>
                         <div>
-                            <img src="Cardinfo/likecommit" alt="" />
+                            <img src="/src/assets/Cardinfo/likecommit.svg" alt="" />
                         </div>
                     </div>
                     <div className='flex gap-[10px] py-[5px]'>
                         <p className='text-[#FD9339] text-[14px]'>+95 <span className='text-[#808080]'>бонусных рублей</span></p>
-                        <img src="Cardinfo/question" alt="" />
+                        <img src="/src/assets/Cardinfo/question.svg" alt="" />
                     </div>
                     <div className='py-[5px]'>
                         <p className='text-[16px]'>Артикул: <span className='text-[#808080]'>23006</span></p>
@@ -50,9 +50,9 @@ function Cardinfo() {
                     </div>
                     <div className='flex justify-between py-[10px]'>
                         <div className='flex justify-center py-[10px] gap-[28px] bg-[#FFFFFF] w-[140px] h-[44px]'>
-                            <img src="Cardinfo/Minus" alt="" />
+                            <img src="/src/assets/Cardinfo/Minus.svg" alt="" />
                             <p>3</p>
-                            <img src="Cardinfo/Plus" alt="" />
+                            <img src="/src/assets/Cardinfo/Plus.svg" alt="" />
                         </div>
                         <div>
                             <button className='bg-[#FD9339] w-[320px] h-[44px] text-[#FFFFFF]' >В корзину</button>
